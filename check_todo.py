d8ea64af6c34cc0ed69e7b3c3bfdb1c6c914e44e
bwildner <bwildner@yahoo.de>
2017-12-04 21:25:19 +0100

    Wechselt das Active Sheet Sucht und markiert die gefundene Zelle

--- check_todo.py
+++ check_todo.py
@@ -3,33 +3,41 @@ import win32com.client
  
 #----------------------------------------------------------------------
 #xl = win32.gencache.EnsureDispatch('Excel.Application')
+print "Prg Start"
 xl = win32com.client.DispatchEx('Excel.Application')
 
-ss = xl.Workbooks.Open('d:/todo.xls', ReadOnly=False, IgnoreReadOnlyRecommended=True)    
-sh = ss.ActiveSheet
+ss = xl.Workbooks.Open('e:/todo.xls', ReadOnly=False, IgnoreReadOnlyRecommended=True)    
+
 erl= ss.Worksheets("Erledigt")
+
+ss.Worksheets("Erledigt").Activate()    
+sh = ss.ActiveSheet
+
+print "Suchen Start"
     
-xl.Visible = True 
-time.sleep(1)    
 
-
+for i in range(1,1500):
     
-
-for i in range(1,50):
     #print sh.Cells(i,8)
-    #print erl.Cells(i,8)
+    print erl.Cells(i,8)
     zelle= str(erl.Cells(i,8))
-    if zelle.find('53000')>0:
+    suche= str(56040)
+    print zelle.find(suche)
+    if zelle.find(suche)>=0:
         
-        print "Suchtext gefunden"+str(erl.Cells(i,8))
+        print "Suchtext gefunden "+zelle
         break
+    else:
+        print suche+" ist Nicht gleich " + zelle
               
-        
-zelle="A"+str(100)    
+
+print "Suchen Ende"        
+xl.Visible = True  
+
+zelle="H"+str(i)    
 xl.Range(zelle).Select()    
-time.sleep(5)    
-    
-#ss.Close(False) 
-#xl.Application.Quit()    
-    
+time.sleep(10)    
+ss.Close(False) 
+xl.Application.Quit()    
+print "Programm Ende"    
  
