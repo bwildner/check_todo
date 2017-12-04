102b96a12d6a137bdd638a2b492743c1da65ed4c
bwildner <bwildner@yahoo.de>
2017-12-04 12:47:34 +0100

    Add files via upload

--- check_todo.py
+++ check_todo.py
@@ -5,19 +5,25 @@ import win32com.client
 #xl = win32.gencache.EnsureDispatch('Excel.Application')
 xl = win32com.client.DispatchEx('Excel.Application')
 
-ss = xl.Workbooks.Open('e:/todo.xls', ReadOnly=False, IgnoreReadOnlyRecommended=True)    
-sh = ss.ActiveSheet    
+ss = xl.Workbooks.Open('d:/todo.xls', ReadOnly=False, IgnoreReadOnlyRecommended=True)    
+sh = ss.ActiveSheet
+erl= ss.Worksheets("Erledigt")
     
 xl.Visible = True 
 time.sleep(1)    
 
+
     
 
-for i in range(1,10):
-    print sh.Cells(i,4)
-    zelle= str(sh.Cells(i,4))
-    if zelle=="53000.0":
-        print "Hello"
+for i in range(1,50):
+    #print sh.Cells(i,8)
+    #print erl.Cells(i,8)
+    zelle= str(erl.Cells(i,8))
+    if zelle.find('53000')>0:
+        
+        print "Suchtext gefunden"+str(erl.Cells(i,8))
+        break
+              
         
 zelle="A"+str(100)    
 xl.Range(zelle).Select()    
